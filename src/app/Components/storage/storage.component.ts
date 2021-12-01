d2cf183e932a5e28d26774b76679620b7e0e38a1
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2021-12-01 10:06:48 +0100

    Add file size

--- src/app/Components/storage/storage.component.ts
+++ src/app/Components/storage/storage.component.ts
@@ -4,32 +4,32 @@ import { File } from 'src/app/Classes/file';
 import { getStorage } from 'firebase/storage';
 
 @Component({
-    selector: 'app-storage',
-    templateUrl: './storage.component.html',
-    styleUrls: ['./storage.component.scss'],
+	selector: 'app-storage',
+	templateUrl: './storage.component.html',
+	styleUrls: ['./storage.component.scss'],
 })
 export class StorageComponent implements OnInit {
-    files: File[] = [];
-    storage = getStorage();
-    searchBar: string;
+	files: File[] = [];
+	storage = getStorage();
+	searchBar: string;
 
-    constructor(private storageService: StorageService) {}
+	constructor(private storageService: StorageService) {}
 
-    ngOnInit(): void {
-        this.getFiles();
-    }
+	ngOnInit(): void {
+		this.getFiles();
+	}
 
-    getFiles() {
-        this.storageService.getFiles().then((files: File[]) => {
-            this.files = files;
-        });
-    }
+	getFiles() {
+		this.storageService.getFiles().then((files: File[]) => {
+			this.files = files;
+		});
+	}
 
-    focusUpload = () => {
-        document.getElementById('file_upload')?.click();
-    };
+	focusUpload = () => {
+		document.getElementById('file_upload')?.click();
+	};
 
-    uploadFile = (event) => {
-        this.storageService.sendFile(event);
-    };
+	uploadFile = (event) => {
+		this.storageService.sendFile(event);
+	};
 }
--- src/app/Services/storage/storage.service.ts
+++ src/app/Services/storage/storage.service.ts
@@ -1,117 +1,118 @@
 import { Injectable } from '@angular/core';
 import { addDoc, collection, getFirestore } from 'firebase/firestore';
 import {
-    getDownloadURL,
-    getStorage,
-    listAll,
-    ref,
-    uploadBytes,
+	getDownloadURL,
+	getStorage,
+	listAll,
+	ref,
+	uploadBytes,
 } from 'firebase/storage';
 import { User } from 'src/app/Classes/user';
 import { File } from 'src/app/Classes/file';
 import Swal from 'sweetalert2';
 
 @Injectable({
-    providedIn: 'root',
+	providedIn: 'root',
 })
 export class StorageService {
-    user: User;
-    files: File[] = [];
+	user: User;
+	files: File[] = [];
 
-    storage = getStorage();
-    db = getFirestore();
+	storage = getStorage();
+	db = getFirestore();
 
-    constructor() {}
+	constructor() {}
 
-    async getFiles(): Promise<File[]> {
-        const filesRef = ref(this.storage, 'files');
+	async getFiles(): Promise<File[]> {
+		const filesRef = ref(this.storage, 'files');
 
-        listAll(filesRef)
-            .then((res) => {
-                res.items.forEach((itemRef) => {
-                    const name = itemRef.name;
-                    const extensionFile = name.split('.')[1];
+		listAll(filesRef)
+			.then((res) => {
+				res.items.forEach((itemRef) => {
+					const name = itemRef.name;
+					const extensionFile = name.split('.')[1];
 
-                    // Get URL file
-                    getDownloadURL(ref(this.storage, `files/${name}`))
-                        .then(async (url) => {
-                            const file = new File(
-                                name,
-                                url,
-                                extensionFile,
-                                new Date()
-                            );
-                            this.files.push(file);
-                        })
-                        .catch((error) => {
-                            console.log('error: ', error);
+					// Get URL file
+					getDownloadURL(ref(this.storage, `files/${name}`))
+						.then(async (url) => {
+							const file = new File(
+								name,
+								url,
+								extensionFile,
+								new Date()
+							);
+							this.files.push(file);
+						})
+						.catch((error) => {
+							console.log('error: ', error);
 
-                            Swal.fire({
-                                position: 'top-end',
-                                icon: 'success',
-                                title: `Error while getting URL files ${error}`,
-                                showConfirmButton: false,
-                                timer: 1500,
-                            });
-                        });
-                });
-            })
-            .catch((error) => {
-                console.log('error: ', error);
+							Swal.fire({
+								position: 'top-end',
+								icon: 'success',
+								title: `Error while getting URL files ${error}`,
+								showConfirmButton: false,
+								timer: 1500,
+							});
+						});
+				});
+			})
+			.catch((error) => {
+				console.log('error: ', error);
 
-                Swal.fire({
-                    position: 'top-end',
-                    icon: 'success',
-                    title: `Error while getting files ${error}`,
-                    showConfirmButton: false,
-                    timer: 1500,
-                });
-            });
+				Swal.fire({
+					position: 'top-end',
+					icon: 'success',
+					title: `Error while getting files ${error}`,
+					showConfirmButton: false,
+					timer: 1500,
+				});
+			});
 
-        return this.files;
-    }
+		return this.files;
+	}
 
-    /**
-     * Send file
-     * @param event
-     */
-    sendFile = (event) => {
-        // Get file
-        const file = event.target.files[0];
-        console.log('file: ', file);
+	/**
+	 * Send file
+	 * @param event
+	 */
+	sendFile = (event) => {
+		// Get file
+		const file = event.target.files[0];
+		console.log('file: ', file);
 
-        const storageRef = ref(this.storage, `files/${file.name}`);
+		const storageRef = ref(this.storage, `files/${file.name}`);
 
-        // Upload to storage
-        uploadBytes(storageRef, file)
-            .then(async (image) => {
-                Swal.fire({
-                    position: 'top-end',
-                    icon: 'success',
-                    title: 'File has been uploaded successfully',
-                    showConfirmButton: false,
-                    timer: 1500,
-                });
+		// Upload to storage
+		uploadBytes(storageRef, file)
+			.then(async (image) => {
+				Swal.fire({
+					position: 'top-end',
+					icon: 'success',
+					title: 'File has been uploaded successfully',
+					showConfirmButton: false,
+					timer: 1500,
+				});
 
-                // Upload to firestore
-                await addDoc(collection(this.db, 'files'), {
-                    name: file.name,
-                    firstName: 'Alex',
-                    lastName: 'Vernet',
-                    email: 'alexandre.vernet@g-mail.fr',
-                    date: new Date(),
-                });
-            })
-            .catch((error) => {
-                console.log('error: ', error);
+				// Upload to firestore
+				await addDoc(collection(this.db, 'files'), {
+					name: file.name,
+					size: file.size,
+					firstName: 'Alex',
+					lastName: 'Vernet',
+					email: 'alexandre.vernet@g-mail.fr',
+					date: new Date(),
+				});
+			})
+			.catch((error) => {
+				console.log('error: ', error);
 
-                Swal.fire({
-                    position: 'top-end',
-                    icon: 'error',
-                    title: `Error ${error}`,
-                    showConfirmButton: false,
-                    timer: 1500,
-                });
-            });
-    };
+				Swal.fire({
+					position: 'top-end',
+					icon: 'error',
+					title: `Error ${error}`,
+					showConfirmButton: false,
+					timer: 1500,
+				});
+			});
+	};
 }
