7600d8c9d8b811afc50b4fc3afe21bd2d161d4cd
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-01-12 18:19:20 +0100

    Optimize code

--- src/app/Components/storage/storage.component.ts
+++ src/app/Components/storage/storage.component.ts
@@ -1,80 +1,69 @@
-import { Component, OnInit } from "@angular/core";
-import { StorageService } from "src/app/Services/storage/storage.service";
-import { File } from "src/app/Classes/file";
-import { getStorage } from "firebase/storage";
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from 'src/app/Services/storage/storage.service';
+import { File } from 'src/app/Classes/file';
+import { getStorage } from 'firebase/storage';
 
 @Component({
-	selector: "app-storage",
-	templateUrl: "./storage.component.html",
-	styleUrls: ["./storage.component.scss"]
+    selector: 'app-storage',
+    templateUrl: './storage.component.html',
+    styleUrls: ['./storage.component.scss']
 })
 export class StorageComponent implements OnInit {
-	storage = getStorage();
-	file: File;
-	files: File[] = [];
-	searchBar: string;
+    storage = getStorage();
+    file: File;
+    files: File[] = [];
+    searchBar: string;
 
-	constructor(private storageService: StorageService) {}
+    constructor(private storageService: StorageService) {
+    }
 
-	ngOnInit(): void {
-		this.getFiles();
-	}
+    ngOnInit(): void {
+        this.getFiles();
+    }
 
-	getFiles() {
-		this.storageService.getFiles().then((files: File[]) => {
-			console.log(files);
-			this.files = files;
-		});
-	}
+    getFiles() {
+        this.storageService.getFiles().then((files: File[]) => {
+            console.log(files);
+            this.files = files;
+        });
+    }
 
-	getIcon(file: File): string {
-		const source = 'assets/images/icons';
-		switch (file.extension) {
-			case 'png':
-			case 'jpg':
-			case 'jpeg':
-				return `${source}/jpg.png`;
-			case 'pdf':
-				return `${source}/pdf.png`;
-			default:
-				return `${source}/bat.png`;
-		}
-	}
+    getIcon(file: File): string {
+        const source = 'assets/images/icons';
+        switch (file.extension) {
+            case 'png':
+            case 'jpg':
+            case 'jpeg':
+                return `${ source }/jpg.png`;
+            case 'pdf':
+                return `${ source }/pdf.png`;
+            default:
+                return `${ source }/bat.png`;
+        }
+    }
 
-	fileDetails(file: File) {
-		this.file = file;
-	}
+    fileDetails(file: File) {
+        this.file = file;
+    }
 
-	focusUpload = () => {
-		document.getElementById('file_upload')?.click();
-	};
+    focusUpload() {
+        document.getElementById('file_upload')?.click();
+    };
 
-	uploadFile = (event) => {
-		this.storageService.uploadFile(event);
-	};
+    uploadFile(event) {
+        this.storageService.uploadFile(event);
+    };
 
-	deleteFile(file: File) {
-		this.storageService.deleteFile(file);
-	}
+    deleteFile(file: File) {
+        this.storageService.deleteFile(file);
+    }
 
-	formatDate = (date) => {
-		const time = date.toDate().toLocaleTimeString('fr-FR');
+    formatDate(date) {
+        return date.toDate().toLocaleTimeString('fr-FR');
+    };
 
-		return time;
-	};
-
-	// Convert octets to kilo octets
-	convertOctets(fileSize: number) {
-		return fileSize / 1000;
-	}
-
-	formatDate2 = (date) => {
-		// const now = new Date();
-		// console.log('now: ', now);
-		// const y = now.getFullYear();
-		// const m = now.getMonth() + 1;
-		// const d = now.getDate();
-		// const formatedDate = `${y}-${m}-${d}`;
-		// return formatedDate;
-	};
+    // Convert octets to kilo octets
+    convertOctets(fileSize: number) {
+        return fileSize / 1000;
+    }
 }
