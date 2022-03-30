465b3a3208b8be1f2ac2c2ade60996fdd4d376eb
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-03-30 15:16:23 +0200

    Feat - Add guard auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,37 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { SignInComponent } from "./Components/sign-in/sign-in.component";
-import { SignUpComponent } from "./Components/sign-up/sign-up.component";
-import { HomeComponent } from "./Components/home/home.component";
-import { AdminGuard } from "./Guards/Admin/admin.guard";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './Components/sign-in/sign-in.component';
+import { SignUpComponent } from './Components/sign-up/sign-up.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AuthGuard } from './Guards/auth/auth.guard';
 
 const routes: Routes = [
-	{ path: "sign-up", component: SignUpComponent },
-	{ path: "sign-in", component: SignInComponent },
-	{
-		path: "storage",
-		loadChildren: () => import("./Components/storage/storage.module").then(module => module.StorageModule)
-	},
-	{
-		path: "admin",
-		loadChildren: () => import("./Components/admin/admin.module").then(module => module.AdminModule),
-		// canLoad: [AdminGuard]
-	},
-  {
-    path: "user-profile",
-    loadChildren: () => import("./Components/user-profile/user-profile.module").then(module => module.UserProfileModule)
-  },
-	{ path: "**", component: HomeComponent }
+    { path: 'sign-up', component: SignUpComponent },
+    { path: 'sign-in', component: SignInComponent },
+    {
+        path: 'storage',
+        loadChildren: () => import('./Components/storage/storage.module').then(module => module.StorageModule),
+        canActivate: [AuthGuard],
+    },
+    {
+        path: 'admin',
+        loadChildren: () => import('./Components/admin/admin.module').then(module => module.AdminModule),
+    },
+    {
+        path: 'user-profile',
+        loadChildren: () => import('./Components/user-profile/user-profile.module').then(module => module.UserProfileModule),
+        canActivate: [AuthGuard],
+    },
+    {
+        path: '**',
+        component: HomeComponent,
+        canActivate: [AuthGuard],
+    }
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
