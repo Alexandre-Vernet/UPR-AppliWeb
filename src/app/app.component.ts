277dba3babfbd157cd83fcf6c05fdbb9332a8179
Arnaud Lefrancois <arn.lefrancois@hotmail.fr>
2022-03-30 16:31:03 +0200

    remove nav and hide admin button in nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from "./Services/authentication/authentication.service";
+import {User_Roles} from "./Classes/user";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
-    constructor() {
+  logged: boolean = false;
+  admin: boolean = false;
+
+    constructor(
+      private authService: AuthenticationService
+    ) {
     }
 
+    ngOnInit() {
+      this.logged = !(window.location.pathname === '/sign-in' || window.location.pathname === '/sign-up');
+      if (this.logged && this.authService.user.role === User_Roles.admin) this.admin = true;
+    }
 
     getRoute() {
         return window.location.pathname;
