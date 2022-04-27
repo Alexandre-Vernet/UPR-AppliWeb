1b7aaec3ee3ab4995c0b4b08594d07f55cf4390a
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-04-27 13:39:33 +0200

    Fix - search pipe

--- src/app/Pipes/search.pipe.spec.ts
+++ src/app/Pipes/search.pipe.spec.ts
@@ -1,4 +1,3 @@
-// @ts-ignore
 import { SearchPipe } from './search.pipe';
 
 describe('SearchPipe', () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,47 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { SignInComponent } from "./Components/sign-in/sign-in.component";
-import { SignUpComponent } from "./Components/sign-up/sign-up.component";
-import { HomeComponent } from "./Components/home/home.component";
-import { ServiceWorkerModule } from "@angular/service-worker";
-import { environment } from "../environments/environment";
-import { CommonModule } from "@angular/common";
-import { NavbarTopComponent } from "./Components/navbar/navbar-top/navbar-top.component";
-import { NavbarMessagesComponent } from "./Components/navbar/navbar-messages/navbar-messages.component";
-import { NavbarSideComponent } from "./Components/navbar/navbar-side/navbar-side.component";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignInComponent } from './Components/sign-in/sign-in.component';
+import { SignUpComponent } from './Components/sign-up/sign-up.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { CommonModule } from '@angular/common';
+import { NavbarTopComponent } from './Components/navbar/navbar-top/navbar-top.component';
+import { NavbarMessagesComponent } from './Components/navbar/navbar-messages/navbar-messages.component';
+import { NavbarSideComponent } from './Components/navbar/navbar-side/navbar-side.component';
 import { CalendarComponent } from './Components/calendar/calendar.component';
 import { HotTableModule } from '@handsontable/angular';
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		SignUpComponent,
-		SignInComponent,
-		HomeComponent,
-		NavbarTopComponent,
-		NavbarMessagesComponent,
-		NavbarSideComponent,
-    CalendarComponent,
-  ],
-	imports: [
-    CommonModule,
-    HotTableModule.forRoot(),
-    BrowserModule,
-		AppRoutingModule,
-		FormsModule,
-		ReactiveFormsModule,
-		ServiceWorkerModule.register('ngsw-worker.js', {
-			enabled: environment.production,
-			// Register the ServiceWorker as soon as the app is stable
-			// or after 30 seconds (whichever comes first).
-			registrationStrategy: 'registerWhenStable:30000',
-		}),
-	],
-	providers: [],
-	bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+        SignUpComponent,
+        SignInComponent,
+        HomeComponent,
+        NavbarTopComponent,
+        NavbarMessagesComponent,
+        NavbarSideComponent,
+        CalendarComponent,
+    ],
+    imports: [
+        CommonModule,
+        HotTableModule.forRoot(),
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: environment.production,
+            // Register the ServiceWorker as soon as the app is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000',
+        }),
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
