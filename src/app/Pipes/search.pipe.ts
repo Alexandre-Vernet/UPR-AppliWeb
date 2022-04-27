74a457b79c4b53bd4afa525e6a7c14236281d353
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-04-27 13:39:39 +0200

    Fix - search pipe

--- src/app/Pipes/search.pipe.ts
+++ src/app/Pipes/search.pipe.ts
@@ -1,13 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { File } from 'src/app/Classes/file';
 
 @Pipe({
-    name: 'search'
+  name: 'search'
 })
 export class SearchPipe implements PipeTransform {
 
-    transform(snapshotArr: any, searchValue: string) {
-        if (searchValue) {
-            return snapshotArr.filter(item => item.name.toLowerCase().includes(searchValue.toLowerCase()));
-        } else return snapshotArr;
-    }
+  transform(files: File[], searchValue: string) {
+    if (searchValue) {
+      return files.filter(item => item.name.toLowerCase().includes(searchValue.toLowerCase()));
+    } else return files;
+  }
+
 }
